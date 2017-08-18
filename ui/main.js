30f020489855b957e2a8618a647357aa93745926
Vigneshukl <30869437+Vigneshukl@users.noreply.github.com>
2017-08-18 06:49:09 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 
-//change the value of maain-text div
+//change the value of main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='Hi! I am Vignesh';
