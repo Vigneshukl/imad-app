1c9e7397fb434757569ff67facf991155d8fb3aa
Vigneshukl <30869437+Vigneshukl@users.noreply.github.com>
2017-08-18 07:05:02 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,11 @@ element.innerHTML='Hi! I am Vignesh';
 //Move the image
 
 var element=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginleft = marginleft + 10;
+    img.style.marginleft=marginleft + 'px';
+}
 img.onclick= function (){
-    img.style.marginleft = '100px';
+    var interval =setInterval(moveRight, 50);
 };
