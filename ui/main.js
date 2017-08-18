ec15edd323c936d99cd4c2bc3b6f2e28b1d64424
Vigneshukl <30869437+Vigneshukl@users.noreply.github.com>
2017-08-18 06:56:17 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //change the value of main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='Hi! I am Vignesh';
+
+//Move the image
+
+var element=document.getElementById('madi');
+img.onclck= function (){
+    img.style.marginleft = '100px';
+};
